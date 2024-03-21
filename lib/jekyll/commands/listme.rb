1615a435a0162c2d18dbf48e839f46391c4c2900
Francesco Pira <dev@fpira.com>
2024-03-21 21:54:38 +0100

    case-insensitive input + validation

--- lib/jekyll/commands/listme.rb
+++ lib/jekyll/commands/listme.rb
@@ -8,7 +8,7 @@ module Jekyll
         def init_with_program(prog)
           prog.command(:list) do |c|
             c.description "List tags and categories used in the site."
-            c.syntax "listme [options]"
+            c.syntax "list [options]"
 
             c.option "tags", "-t", "--tags", "List tags"
             c.option "categories", "-c", "--categories", "List categories"
@@ -21,28 +21,49 @@ module Jekyll
           end
         end
 
-        def process(args, opts)
+        def normalize_output_format(o)
+          if o.nil? then
+            o == "plain"
+          else
+            o = o.downcase
+          end
+          o
+        end
+
+        def get_site(opts)
           Jekyll.logger.adjust_verbosity(opts)
           options = configuration_from_options(opts)
           site = Jekyll::Site.new(options)
-
           site.reset
           site.read
+          site
+        end
 
+        def process(args, opts)
+          opts["output"] = normalize_output_format(opts["output"])
+          supported_formats = [ "plain", "yaml", "json", "csv", "tsv", "psv", "toml" ]
+          unless supported_formats.include?(opts["output"])
+            Jekyll.logger.error "Invalid output format. Supported formats are: #{supported_formats.join(", ")}"
+            return
+          end
+
+          if opts["tags"].nil? && opts["categories"].nil? then
+            Jekyll.logger.error "You must specify either --tags or --categories."
+            return
+          end
+
+          site = get_site(opts)
           list = nil
           if opts["tags"]
             list = get_tags(site)
-          elsif opts["categories"]
-            list = get_categories(site)
           else
-            Jekyll.logger.error "You must specify either --tags or --categories."
-            return
+            list = get_categories(site)
           end
           print_data(list, opts)
         end
 
         def print_data(data, opts)
-          print_list_text(data) if opts["output"].nil? || opts["output"] == "plain"
+          print_list_text(data) if opts["output"] == "plain"
           print_list_yaml(data) if opts["output"] == "yaml"
           print_list_json(data) if opts["output"] == "json"
           print_list_text(data, ",") if opts["output"] == "csv"
@@ -57,7 +78,6 @@ module Jekyll
 
         def get_tags(site)
           tags = Hash.new(0)
-
           # Loop through all the posts
           site.posts.docs.each do |post|
             # Loop through each tag of the post
